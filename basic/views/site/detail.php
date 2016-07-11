31c4131d7be4a27d37aff09ecfae7d36233f2b24
Rebecca Santoro <rls354@drexel.edu>
2016-07-11 16:19:13 -0400

    detail

---
+++ basic/views/site/detail.php
@@ -0,0 +1,34 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
+
+<div class="site-detail">
+
+    <div class="jumbotron">
+        
+
+        <p class="lead">Title</p>
+
+        
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+            <div class="col-lg-4">
+                <h2>Heading</h2>
+
+                <p></p>
+
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+            </div>
+        </div>
+
+    </div>
+</div>
