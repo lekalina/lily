0e25641ecf7bf25af78f6f65d3acf0fc15606d90
lek <lekalina@gmail.com>
2016-07-12 16:26:42 -0400

    removed the title

--- basic/views/site/gallery.php
+++ basic/views/site/gallery.php
@@ -11,7 +11,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
 ?>
 <div class="site-about">
-    <h1><?= Html::encode($this->title) ?></h1>
+    <!--<h1><?php //Html::encode($this->title) ?></h1>-->
 
     <div class="row">
         <?php foreach($images as $image): ?>
@@ -24,5 +24,5 @@ $this->params['breadcrumbs'][] = $this->title;
         <?php endforeach ?>
     </div>
 
-    <code><?= __FILE__ ?></code>
+    <!--<code><?php // __FILE__ ?></code>-->
 </div>
