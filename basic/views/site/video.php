6fd31a2e4dc5f488c4ccff1406776fd7dcd8cebc
lek <lekalina@gmail.com>
2016-07-15 14:01:19 -0400

    removed video border

--- basic/views/site/video.php
+++ basic/views/site/video.php
@@ -7,31 +7,31 @@ and open the template in the editor.
 
 <div class="col-lg-12" align="center">
     
-    <iframe width="350" height="600"
+    <iframe width="350" height="600" frameborder="0"
         src="https://www.youtube.com/embed/fM6v4i5c-hY">
     </iframe>
-    <iframe width="350" height="600"
+    <iframe width="350" height="600" frameborder="0"
         src="https://www.youtube.com/embed/2mGphUbfifc">
     </iframe>
-    <iframe width="350" height="600"
+    <iframe width="350" height="600" frameborder="0"
         src="https://www.youtube.com/embed/ynIi3SNaKBU">
     </iframe>
-    <iframe width="350" height="600"
+    <iframe width="350" height="600" frameborder="0"
         src="https://www.youtube.com/embed/FBiW6OhdFmE">
     </iframe>
-    <iframe width="350" height="600"
+    <iframe width="350" height="600" frameborder="0"
         src="https://www.youtube.com/embed/DsxNHXhRLyg">
     </iframe>
-    <iframe width="350" height="600"
+    <iframe width="350" height="600" frameborder="0"
         src="https://www.youtube.com/embed/3Ua2QxrBZo8">
     </iframe>
-    <iframe width="350" height="600"
+    <iframe width="350" height="600" frameborder="0"
         src="https://www.youtube.com/embed/QW5o_vV5FbA">
     </iframe>
-    <iframe width="350" height="600"
+    <iframe width="350" height="600" frameborder="0"
         src="https://www.youtube.com/embed/sIbIUl8cOug">
     </iframe>
-    <iframe width="350" height="600"
+    <iframe width="350" height="600" frameborder="0"
         src="https://www.youtube.com/embed/-dBMzVX9FKE">
     </iframe>
     
